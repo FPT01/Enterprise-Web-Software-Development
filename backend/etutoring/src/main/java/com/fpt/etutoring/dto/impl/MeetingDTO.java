3b96eefa718084760530589261efdbe9fb2225b7
phonglhtcs19002 <61347858+phonglhtcs19002@users.noreply.github.com>
2020-03-04 20:34:46 +0700

    CRUD meeting

--- backend/etutoring/src/main/java/com/fpt/etutoring/dto/impl/MeetingDTO.java
+++ backend/etutoring/src/main/java/com/fpt/etutoring/dto/impl/MeetingDTO.java
@@ -1,12 +1,11 @@
 package com.fpt.etutoring.dto.impl;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fpt.etutoring.dto.BaseDTO;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Getter
 @Setter
@@ -14,7 +13,9 @@ public class MeetingDTO extends BaseDTO {
     private  Long id;
     private String title;
     private String description;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
     private Date creationTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
     private Date modifiedTime;
-    private Set<UserDTO> users = new HashSet<>(0);
+    private List<UserDTO> userDTOS;
 }
