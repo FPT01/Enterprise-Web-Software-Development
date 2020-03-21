ee413c98a4dfc3a28cfa5ac3edfcfd35d54c0a47
phonglhtcs19002 <61347858+phonglhtcs19002@users.noreply.github.com>
2020-03-21 10:27:55 +0700

    chat demo

--- backend/etutoring/src/main/java/com/fpt/etutoring/util/Constant.java
+++ backend/etutoring/src/main/java/com/fpt/etutoring/util/Constant.java
@@ -38,4 +38,11 @@ public class Constant {
     public static final String PATH_STATISTIC = "/statistic";
     public static final String PATH_LAST_SEVEN_DAYS = "/lastsevendays";
     public static final String PATH_AVG_MSG = "/avgmsg";
+    public static final String PATH_SEND_MSG = "/sendMessage";
+    public static final String PATH_TOPIC_PUBLIC = "/topic/pubic";
+    public static final String PATH_ADD_USER = "/addUser";
+    public static final String PATH_SEND_PRIVATE_MSG = "/sendPrivateMessage";
+    public static final String PATH_REPLY = "/reply";
+    public static final String PATH_ADD_PRIVATE_USER = "/addPrivateUser";
+    public static final String PATH_QUEUE_REPLY = "/queue" + PATH_REPLY;
 }
