70f549e914d8ea6fb116d166c629cdf79ea2dd36
phonglhtcs19002 <61347858+phonglhtcs19002@users.noreply.github.com>
2020-03-23 08:33:33 +0700

    Update BlogPostController.java

--- backend/etutoring/src/main/java/com/fpt/etutoring/controller/impl/BlogPostController.java
+++ backend/etutoring/src/main/java/com/fpt/etutoring/controller/impl/BlogPostController.java
@@ -2,6 +2,7 @@ package com.fpt.etutoring.controller.impl;
 
 import com.fpt.etutoring.controller.BaseController;
 import com.fpt.etutoring.dto.ResponseDTO;
+import com.fpt.etutoring.dto.impl.BlogCommentDTO;
 import com.fpt.etutoring.dto.impl.BlogPostDTO;
 import com.fpt.etutoring.dto.impl.UserDTO;
 import com.fpt.etutoring.entity.impl.BlogPost;
@@ -17,7 +18,9 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping(Constant.PATH_BLOG_POST)
@@ -42,7 +45,18 @@ public class BlogPostController implements BaseController<BlogPostDTO, Long> {
                     UserDTO userDTO = ResponseDTO.accepted().getObject(u, UserDTO.class);
                     blogPostDTO.setUser(userDTO);
                 }
-
+                if (!CollectionUtils.isEmpty(b.getBlogComments())) {
+                    Set<BlogCommentDTO> blogCommentDTOS = new HashSet<>();
+                    b.getBlogComments().forEach(c -> {
+                        if (c.getUser() != null) {
+                            Role r = c.getUser().getRole();
+                            r.setUsers(null);
+                            c.getUser().setRole(r);
+                        }
+                        blogCommentDTOS.add(ResponseDTO.accepted().getObject(c, BlogCommentDTO.class));
+                    });
+                    blogPostDTO.setBlogComments(blogCommentDTOS);
+                }
                 blogPostDTOS.add(blogPostDTO);
             });
         }
