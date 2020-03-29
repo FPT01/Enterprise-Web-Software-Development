5b98df7259bb2ea7e905de34fb3f13a0a700ce7e
phonglhtcs19002 <61347858+phonglhtcs19002@users.noreply.github.com>
2020-03-29 10:54:29 +0700

    connected web socket

--- backend/etutoring/src/main/java/com/fpt/etutoring/config/WebSocketConfig.java
+++ backend/etutoring/src/main/java/com/fpt/etutoring/config/WebSocketConfig.java
@@ -1,18 +1,30 @@
 package com.fpt.etutoring.config;
 
-import com.fpt.etutoring.websocket.SocketHandler;
+import com.fpt.etutoring.websocket.HttpHandshakeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private HttpHandshakeInterceptor handshakeInterceptor;
+
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SocketHandler(), "/ws");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS().setInterceptors(handshakeInterceptor);
     }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+
 }
--- backend/etutoring/src/main/resources/static/app.js
+++ backend/etutoring/src/main/resources/static/app.js
@@ -1,48 +1,9 @@
-
-var ws;
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
-}
+'use strict';
 
 function connect() {
-	ws = new WebSocket('ws://localhost:8080/ws');
-	ws.onmessage = function(data){
-		showGreeting(data.data);
-	}
-	 setConnected(true);
+    var socket = new SockJS('/ws');
+    var stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, onConnected, onError);
+    console.log(">>>>>>>>> connected");
 }
-
-function disconnect() {
-    if (ws != null) {
-        ws.close();
-    }
-    setConnected(false);
-    console.log("Disconnected");
-}
-
-function sendName() {
-	var data = JSON.stringify({'name': $("#name").val()})
-    ws.send(data);
-}
-
-function showGreeting(message) {
-    $("#greetings").append("<tr><td> " + message + "</td></tr>");
-}
-
-$(function () {
-    $("form").on('submit', function (e) {
-        e.preventDefault();
-    });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
-});
-
