347ad28f01c702d2da4fd02296074ecccecf587a
phonglhtcs19002 <61347858+phonglhtcs19002@users.noreply.github.com>
2020-03-30 15:20:33 +0700

    Update WebMvcConfig.java

--- backend/etutoring/src/main/java/com/fpt/etutoring/config/WebMvcConfig.java
+++ backend/etutoring/src/main/java/com/fpt/etutoring/config/WebMvcConfig.java
@@ -3,15 +3,23 @@ package com.fpt.etutoring.config;
 import com.fpt.etutoring.dto.DTOMethodProcessor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new DTOMethodProcessor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
 }
