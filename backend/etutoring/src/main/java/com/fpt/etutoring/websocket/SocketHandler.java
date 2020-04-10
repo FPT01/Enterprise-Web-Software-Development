51451bacb47497638c2094ba8e179ceaa5dcf5bb
phonglhtcs19002 <61347858+phonglhtcs19002@users.noreply.github.com>
2020-04-10 08:32:30 +0700

    update chat

--- backend/etutoring/src/main/java/com/fpt/etutoring/websocket/SocketHandler.java
+++ backend/etutoring/src/main/java/com/fpt/etutoring/websocket/SocketHandler.java
@@ -1,16 +1,16 @@
+/*
 package com.fpt.etutoring.websocket;
 
 import com.fpt.etutoring.entity.impl.Message;
 import com.fpt.etutoring.entity.impl.User;
-import com.fpt.etutoring.service.MessageService;
-import com.fpt.etutoring.service.UserService;
 import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -18,12 +18,14 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class SocketHandler extends TextWebSocketHandler {
-    @Autowired
-    private MessageService messageService;
-    @Autowired
-    private UserService userService;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    private User findById(long id) {
+        return entityManager.find(User.class, id);
+    }
 
     @Override
     public synchronized void handleTextMessage(WebSocketSession session, TextMessage message)
@@ -35,11 +37,11 @@ public class SocketHandler extends TextWebSocketHandler {
         Message msg = new Message();
         Long userId = Long.valueOf(value.get("userId"));
         if(userId != null) {
-            User user = userService.findById(userId);
+            User user = findById(userId);
             msg.setUser(user);
         }
         msg.setContent(value.get("msg"));
-        messageService.createOrUpdate(msg);
+        //messageService.createOrUpdate(msg);
         session.sendMessage(new TextMessage(value.get("msg")));
     }
 
@@ -49,3 +51,4 @@ public class SocketHandler extends TextWebSocketHandler {
         sessions.add(session);
     }
 }
+*/
