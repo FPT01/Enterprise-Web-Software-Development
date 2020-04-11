0b59056d89c05510ae95fa430bffa3722a69b468
phonglhtcs19002 <61347858+phonglhtcs19002@users.noreply.github.com>
2020-04-11 13:44:53 +0700

    Update ChatController.java

--- backend/etutoring/src/main/java/com/fpt/etutoring/controller/impl/ChatController.java
+++ backend/etutoring/src/main/java/com/fpt/etutoring/controller/impl/ChatController.java
@@ -1,12 +1,18 @@
 package com.fpt.etutoring.controller.impl;
 
+import com.fpt.etutoring.entity.impl.User;
 import com.fpt.etutoring.service.MessageService;
 import com.fpt.etutoring.service.UserService;
+import com.fpt.etutoring.websocket.Message;
+import com.fpt.etutoring.websocket.OutputMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 @CrossOrigin(value = "*")
@@ -16,28 +22,21 @@ public class ChatController {
     @Autowired
     private MessageService messageService;
 
-    private final SimpMessagingTemplate simpMessagingTemplate;
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public synchronized OutputMessage send(final Message message) throws Exception {
+        User user = userService.findByUsername(message.getFrom());
+        if (user == null)
+            return new OutputMessage();
 
-    public ChatController(SimpMessagingTemplate simpMessagingTemplate) {
-        this.simpMessagingTemplate = simpMessagingTemplate;
-    }
+        Date date = new Date();
+        final String time = new SimpleDateFormat("HH:mm").format(date);
 
-    @MessageMapping("/greetings")
-    public synchronized void send(String message) throws Exception {
-//        User user = userService.findByUsername(message.getFrom());
-//        if (user == null)
-//            return new OutputMessage();
-//
-//        Date date = new Date();
-//        final String time = new SimpleDateFormat("HH:mm").format(date);
-//
-//        com.fpt.etutoring.entity.impl.Message msg = new com.fpt.etutoring.entity.impl.Message();
-//        msg.setUser(user);
-//        msg.setContent(message.getText());
-//        msg.setTime(date);
-//        messageService.createOrUpdate(msg);
-//        return new OutputMessage(message.getFrom(), message.getText(), time);
-        String text = "Hi! " + message;
-        this.simpMessagingTemplate.convertAndSend("/topic/greetings", text);
+        com.fpt.etutoring.entity.impl.Message msg = new com.fpt.etutoring.entity.impl.Message();
+        msg.setUser(user);
+        msg.setContent(message.getText());
+        msg.setTime(date);
+        messageService.createOrUpdate(msg);
+        return new OutputMessage(message.getFrom(), message.getText(), time);
     }
 }
