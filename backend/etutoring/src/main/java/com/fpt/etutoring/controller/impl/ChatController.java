2e4d1a161b311b78bd826462c0dd630734c48232
phonglhtcs19002 <61347858+phonglhtcs19002@users.noreply.github.com>
2020-04-12 21:24:19 +0700

    update

--- backend/etutoring/src/main/java/com/fpt/etutoring/controller/impl/ChatController.java
+++ backend/etutoring/src/main/java/com/fpt/etutoring/controller/impl/ChatController.java
@@ -11,13 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Controller
 @CrossOrigin(value = "*")
@@ -36,7 +33,7 @@ public class ChatController {
     }
 
     @MessageMapping(Constant.CHAT_GREETINGS)
-    public synchronized void send(String message) throws Exception {
+    public void send(String message) throws Exception {
         JsonObject convertedObject = new Gson().fromJson(message, JsonObject.class);
         User user = userService.findByUsername(convertedObject.get("username").getAsString());
         Date date = new Date();
@@ -50,19 +47,5 @@ public class ChatController {
         OutputMessage output = new OutputMessage(convertedObject.get("username").getAsString(),
                 convertedObject.get("text").getAsString(), time);
         this.simpMessagingTemplate.convertAndSend(Constant.CHAT_TOPIC_GREETINGS, output);
-    }
-
-    @MessageMapping(Constant.CHAT_GET_ALL)
-    public void getAll() throws Exception {
-        List<com.fpt.etutoring.entity.impl.Message> messageList = messageService.list();
-        List<OutputMessage> outputMessages = new ArrayList<>();
-        if (!CollectionUtils.isEmpty(messageList)) {
-            messageList.forEach(m -> {
-                OutputMessage out = new OutputMessage(m.getUser().getUsername(),
-                        m.getContent(), dateFormat.format(m.getTime()));
-                outputMessages.add(out);
-            });
-        }
-        this.simpMessagingTemplate.convertAndSend(Constant.CHAT_TOPIC_GETALL, outputMessages);
     }
 }
