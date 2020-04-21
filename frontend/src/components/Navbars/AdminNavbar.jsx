a603ac4dcbab4093f27441b2f7295720f0e4685e
huedinh <huedtl@go-viet.vn>
2020-04-21 22:26:07 +0700

    optimize code

--- frontend/src/components/Navbars/AdminNavbar.jsx
+++ frontend/src/components/Navbars/AdminNavbar.jsx
@@ -12,8 +12,6 @@ import React, { Component } from "react";
 import { Navbar } from "react-bootstrap";
 
 import AdminNavbarLinks from "./AdminNavbarLinks.jsx";
-import StudentNavbarLinks from "./StudentNavBarLinks.jsx";
-import TutorNavbarLinks from "./TutorNavbarLinks.jsx";
 
 class Header extends Component {
   constructor(props) {
