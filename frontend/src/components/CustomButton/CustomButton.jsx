f1d44ffbf8e3256f05be1dab38bff9c6a3dd9c29
huedinh <huedtl@go-viet.vn>
2020-04-29 22:36:26 +0700

    update code

---
+++ frontend/src/components/CustomButton/CustomButton.jsx
@@ -0,0 +1,40 @@
+/*!
+
+ =========================================================
+ * Enterprise Web Software Development
+ * Based on Light Bootstrap Dashboard React - v1.3.0
+ * Based on Light Bootstrap Dashboard - v1.3.0
+ =========================================================
+
+*/
+
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import cx from "classnames";
+import PropTypes from "prop-types";
+
+class CustomButton extends Component {
+  render() {
+    const { fill, simple, pullRight, round, block, ...rest } = this.props;
+
+    const btnClasses = cx({
+      "btn-fill": fill,
+      "btn-simple": simple,
+      "pull-right": pullRight,
+      "btn-block": block,
+      "btn-round": round
+    });
+
+    return <Button className={btnClasses} {...rest} />;
+  }
+}
+
+CustomButton.propTypes = {
+  fill: PropTypes.bool,
+  simple: PropTypes.bool,
+  pullRight: PropTypes.bool,
+  block: PropTypes.bool,
+  round: PropTypes.bool
+};
+
+export default CustomButton;
